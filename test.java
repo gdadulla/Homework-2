f7027c39e63eb933e8f1c0fc6aca6641c921412f
gdadulla <gdadulla@albany.edu>
2017-02-25 18:33:41 -0500

    adds test class, main method, and a scanner
    Scanner takes inputs for x and accepted error

---
+++ test.java
@@ -0,0 +1,13 @@
+package Homework2;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main (String [] args){
+		Scanner s = new Scanner(System.in);
+		double x = Double.parseDouble(s.nextLine());
+		int aError = Integer.parseInt(s.nextLine());
+		
+	}
+}
